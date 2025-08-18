58dee3e2baf23fcf94059454f6514f88885969fa
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2025-08-18 22:27:36 +0300

    Solve Practice Design problem 2 variant B

--- practice_design_problem2/src/ILoIntegers.java
+++ practice_design_problem2/src/ILoIntegers.java
@@ -1,11 +1,16 @@
 import tester.Tester;
 
 interface ILoIntegers {
-	boolean checkRequirements();
+	boolean checkRequirementsA();
 
 	// Suggested by ChatGPT
-	boolean checkRequirementsHelper(boolean thereIsAnEvenNumber, boolean thereIsAPositiveOddNumber,
+	boolean checkRequirementsHelperA(boolean thereIsAnEvenNumber, boolean thereIsAPositiveOddNumber,
 			boolean thereIsANumberBetween5and10);
+
+	boolean checkRequirementsB();
+
+	boolean checkRequirementsHelperB(boolean thereIsAnEvenNumber, boolean thereIsAPositiveOddNumber,
+			boolean thereIsANumberBetween5and10, int count);
 }
 
 class ConsLoIntegers implements ILoIntegers {
@@ -17,12 +22,12 @@ class ConsLoIntegers implements ILoIntegers {
 		this.rest = rest;
 	}
 
-	public boolean checkRequirements() {
-		return checkRequirementsHelper(false, false, false);
+	public boolean checkRequirementsA() {
+		return checkRequirementsHelperA(false, false, false);
 	}
 
 	@Override
-	public boolean checkRequirementsHelper(boolean thereIsAnEvenNumber, boolean thereIsAPositiveOddNumber,
+	public boolean checkRequirementsHelperA(boolean thereIsAnEvenNumber, boolean thereIsAPositiveOddNumber,
 			boolean thereIsANumberBetween5and10) {
 		// Suggested by ChatGPT
 		if (thereIsAnEvenNumber && thereIsAPositiveOddNumber && thereIsANumberBetween5and10) {
@@ -31,22 +36,49 @@ class ConsLoIntegers implements ILoIntegers {
 			thereIsAnEvenNumber = thereIsAnEvenNumber || first % 2 == 0;
 			thereIsAPositiveOddNumber = thereIsAPositiveOddNumber || (first % 2 != 0 && first > 0);
 			thereIsANumberBetween5and10 = thereIsANumberBetween5and10 || (first >= 5 && first <= 10);
-			return (thereIsAnEvenNumber && thereIsAPositiveOddNumber && thereIsANumberBetween5and10)
-					|| rest.checkRequirementsHelper(thereIsAnEvenNumber, thereIsAPositiveOddNumber,
-							thereIsANumberBetween5and10);
+			return rest.checkRequirementsHelperA(thereIsAnEvenNumber, thereIsAPositiveOddNumber,
+					thereIsANumberBetween5and10);
+		}
+	}
+
+	public boolean checkRequirementsB() {
+		return checkRequirementsHelperB(false, false, false, 0);
+	}
+
+	public boolean checkRequirementsHelperB(boolean thereIsAnEvenNumber, boolean thereIsAPositiveOddNumber,
+			boolean thereIsANumberBetween5and10, int count) {
+		// Suggested by ChatGPT
+		if (thereIsAnEvenNumber && thereIsAPositiveOddNumber && thereIsANumberBetween5and10) {
+			return true;
+		} else {
+			thereIsAnEvenNumber = thereIsAnEvenNumber || first % 2 == 0;
+			thereIsAPositiveOddNumber = thereIsAPositiveOddNumber || (first % 2 != 0 && first > 0);
+			thereIsANumberBetween5and10 = thereIsANumberBetween5and10 || (first >= 5 && first <= 10);
+			return rest.checkRequirementsHelperB(thereIsAnEvenNumber, thereIsAPositiveOddNumber,
+					thereIsANumberBetween5and10, count++);
 		}
 	}
 }
 
 class EmptyLoIntegers implements ILoIntegers {
-	public boolean checkRequirements() {
+	public boolean checkRequirementsA() {
 		return false;
 	}
 
 	@Override
-	public boolean checkRequirementsHelper(boolean thereIsAnEvenNumber, boolean thereIsAPositiveOddNumber,
+	public boolean checkRequirementsHelperA(boolean thereIsAnEvenNumber, boolean thereIsAPositiveOddNumber,
 			boolean thereIsANumberBetween5and10) {
+		return thereIsAnEvenNumber && thereIsAPositiveOddNumber && thereIsANumberBetween5and10;
+	}
+
+	public boolean checkRequirementsB() {
 		return false;
+	}
+
+	@Override
+	public boolean checkRequirementsHelperB(boolean thereIsAnEvenNumber, boolean thereIsAPositiveOddNumber,
+			boolean thereIsANumberBetween5and10, int count) {
+		return thereIsAnEvenNumber && thereIsAPositiveOddNumber && thereIsANumberBetween5and10 && count >= 3;
 	}
 }
 
@@ -54,8 +86,13 @@ class ExamplesILoIntegers {
 	ILoIntegers eli = new EmptyLoIntegers();
 	ILoIntegers list1 = new ConsLoIntegers(6, new ConsLoIntegers(5, eli));
 	ILoIntegers list2 = new ConsLoIntegers(4, new ConsLoIntegers(3, eli));
+	ILoIntegers list3 = new ConsLoIntegers(6, new ConsLoIntegers(5, new ConsLoIntegers(6, eli)));
 
-	boolean testCheckRequirements(Tester t) {
-		return t.checkExpect(list1.checkRequirements(), true) && t.checkExpect(list2.checkRequirements(), false);
+	boolean testCheckRequirementsA(Tester t) {
+		return t.checkExpect(list1.checkRequirementsA(), true) && t.checkExpect(list2.checkRequirementsA(), false);
+	}
+
+	boolean testCheckRequirementsB(Tester t) {
+		return t.checkExpect(list1.checkRequirementsB(), false) && t.checkExpect(list3.checkRequirementsB(), true);
 	}
 }
